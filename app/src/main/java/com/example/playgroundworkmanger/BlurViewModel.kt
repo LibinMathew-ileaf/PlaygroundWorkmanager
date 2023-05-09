2802706eca9752b32fe65f507a1bb6ed329fb0b9
ileaf <libin.m@ileafsolutions.net>
2023-05-09 11:14:21 +0530

    initial project implementations blur images

---
+++ app/src/main/java/com/example/playgroundworkmanger/BlurViewModel.kt
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.playgroundworkmanger
+
+import android.app.Application
+import android.content.ContentResolver
+import android.content.Context
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
+import com.example.playgroundworkmanger.worker.BlurWorker
+import com.example.playgroundworkmanger.worker.IMAGE_MANIPULATION_WORK_NAME
+import com.example.playgroundworkmanger.worker.KEY_IMAGE_URI
+import com.example.playgroundworkmanger.worker.TAG_OUTPUT
+
+
+class BlurViewModel(application: Application) : ViewModel() {
+
+    private var imageUri: Uri? = null
+    internal var outputUri: Uri? = null
+    private val workManager = WorkManager.getInstance(application)
+    internal val outputWorkInfos: LiveData<List<WorkInfo>> = workManager.getWorkInfosByTagLiveData(TAG_OUTPUT)
+
+    init {
+        // This transformation makes sure that whenever the current work Id changes the WorkInfo
+        // the UI is listening to changes
+        imageUri = getImageUri(application.applicationContext)
+    }
+
+    internal fun cancelWork() {
+        workManager.cancelUniqueWork(IMAGE_MANIPULATION_WORK_NAME)
+    }
+
+    /**
+     * Creates the input data bundle which includes the Uri to operate on
+     * @return Data which contains the Image Uri as a String
+     */
+    private fun createInputDataForUri(): Data {
+        val builder = Data.Builder()
+        imageUri?.let {
+            builder.putString(KEY_IMAGE_URI, imageUri.toString())
+        }
+        return builder.build()
+    }
+
+    /**
+     * Create the WorkRequest to apply the blur and save the resulting image
+     * @param blurLevel The amount to blur the image
+     */
+    internal fun applyBlur(blurLevel: Int) {
+        // Add WorkRequest to Cleanup temporary images
+
+        workManager.enqueue(OneTimeWorkRequest.from(BlurWorker::class.java))
+
+    }
+
+    private fun uriOrNull(uriString: String?): Uri? {
+        return if (!uriString.isNullOrEmpty()) {
+            Uri.parse(uriString)
+        } else {
+            null
+        }
+    }
+
+    private fun getImageUri(context: Context): Uri {
+        val resources = context.resources
+
+        return Uri.Builder()
+            .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+            .authority(resources.getResourcePackageName(R.drawable.android_cupcake))
+            .appendPath(resources.getResourceTypeName(R.drawable.android_cupcake))
+            .appendPath(resources.getResourceEntryName(R.drawable.android_cupcake))
+            .build()
+    }
+
+    internal fun setOutputUri(outputImageUri: String?) {
+        outputUri = uriOrNull(outputImageUri)
+    }
+}
+
+class BlurViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(BlurViewModel::class.java)) {
+            BlurViewModel(application) as T
+        } else {
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
+}
